c664b2e2e3558f92490524f13a92ef5d7e9fe310
hsun <123@gmail.com>
2024-10-28 03:19:36 +0800

    only navbar and six charts

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,20 @@
         <v-card>
           <v-container>
             <h1>COVID-19 Tracking Dashboard</h1>
-            <v-row v-if="arrPositive.length">  // 修改以下的部分
+            <v-tabs v-model="tab" background-color="primary" dark>
+              <v-tab v-for="item in items" :key="item.tab">
+                {{ item.tab }}
+              </v-tab>
+            </v-tabs>
+
+            <v-tabs-items v-model="tab">
+              <v-tab-item v-for="item in items" :key="item.tab">
+                <v-card flat>
+                  <v-card-text>{{ item.content }}</v-card-text>
+                </v-card>
+              </v-tab-item>
+            </v-tabs-items>
+            <v-row v-if="arrPositive.length">
               <v-col
                 cols="12"
                 v-for="chartData in renderData"
@@ -39,7 +52,7 @@ export default {
   data() {
     return {
       arrPositive: [],
-      arrHoptialized: [],
+      arrHospitalized: [],
       arrInIcu: [],
       arrOnVentilators: [],
       arrRecovered: [],
@@ -48,6 +61,19 @@ export default {
         responsive: true,
         maintainAspectRatio: false,
       },
+      tab: null,
+      items: [
+          { tab: 'One', content: 'Tab 1 Content' },
+          { tab: 'Two', content: 'Tab 2 Content' },
+          { tab: 'Three', content: 'Tab 3 Content' },
+          { tab: 'Four', content: 'Tab 4 Content' },
+          { tab: 'Five', content: 'Tab 5 Content' },
+          { tab: 'Six', content: 'Tab 6 Content' },
+          { tab: 'Seven', content: 'Tab 7 Content' },
+          { tab: 'Eight', content: 'Tab 8 Content' },
+          { tab: 'Nine', content: 'Tab 9 Content' },
+          { tab: 'Ten', content: 'Tab 10 Content' },
+      ]
     }
   },
   computed: {
@@ -62,7 +88,7 @@ export default {
       ]
       const dataArrays = [
         this.arrPositive,
-        this.arrHoptialized,
+        this.arrHospitalized,
         this.arrInIcu,
         this.arrOnVentilators,
         this.arrRecovered,
@@ -89,7 +115,7 @@ export default {
     data.forEach((item) => {
       const date = dayjs(`${item.date}`).format('YYYY/MM/DD')
       this.arrPositive.push({ date, total: item.positive })
-      this.arrHoptialized.push({ date, total: item.hospitalizedCurrently })
+      this.arrHospitalized.push({ date, total: item.hospitalizedCurrently })
       this.arrInIcu.push({ date, total: item.inIcuCurrently })
       this.arrOnVentilators.push({ date, total: item.onVentilatorCurrently })
       this.arrRecovered.push({ date, total: item.recovered })
--- src/main.js
+++ src/main.js
@@ -12,9 +12,13 @@ import App from './App.vue'
 
 // Composables
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'  // 新的樣式導入方式
 
 const app = createApp(App)
+const vuetify = createVuetify()  // 創建 Vuetify 實例
 
+app.use(vuetify)
 registerPlugins(app)
 
 app.mount('#app')
